de23181b38225f80cf42abff7eec83ff84b6dfa5
lattechiffon <lattechiffon@gmail.com>
2021-07-26 23:39:14 +0900

    revise grpc config

--- src/main/kotlin/com/choulatte/scentuser/config/GrpcClientConfig.kt
+++ src/main/kotlin/com/choulatte/scentuser/config/GrpcClientConfig.kt
@@ -2,18 +2,32 @@ package com.choulatte.scentuser.config
 
 import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class GrpcClientConfig {
+class GrpcClientConfig(
+    @Value("\${grpc.pay.server.name}")
+    var grpcPayServer: String,
+
+    @Value("\${grpc.pay.server.port}")
+    var grpcPayServerPort: Int,
+
+    @Value("\${grpc.product.server.name}")
+    var grpcProductServer: String,
+
+    @Value("\${grpc.product.server.port}")
+    var grpcProductServerPort: Int
+) {
+
     @Bean(name = ["pay"])
     fun setPayChannel(): ManagedChannel {
-        return ManagedChannelBuilder.forAddress("172.20.10.3", 8090).usePlaintext().build()
+        return ManagedChannelBuilder.forAddress(grpcPayServer, grpcPayServerPort).usePlaintext().build()
     }
 
     @Bean(name = ["product"])
     fun setProductChannel(): ManagedChannel {
-        return ManagedChannelBuilder.forAddress("172.20.10.4", 8091).usePlaintext().build()
+        return ManagedChannelBuilder.forAddress(grpcProductServer, grpcProductServerPort).usePlaintext().build()
     }
 }
