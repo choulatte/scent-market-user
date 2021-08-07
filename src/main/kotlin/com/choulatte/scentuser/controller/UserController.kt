0e5bafd12f56331bc4e5696423e9dae387253c34
lattechiffon <lattechiffon@gmail.com>
2021-08-07 23:32:42 +0900

    add api description in swagger

--- src/main/kotlin/com/choulatte/scentuser/controller/UserController.kt
+++ src/main/kotlin/com/choulatte/scentuser/controller/UserController.kt
@@ -5,6 +5,7 @@ import com.choulatte.scentuser.config.JwtTokenProvider
 import com.choulatte.scentuser.dto.LoginDTO
 import com.choulatte.scentuser.dto.TokenDTO
 import com.choulatte.scentuser.dto.UserDTO
+import io.swagger.annotations.ApiOperation
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -15,6 +16,7 @@ class UserController(
     private val jwtTokenProvider: JwtTokenProvider
 ) {
     @PostMapping(value = [""])
+    @ApiOperation(value = "사용자 인증", notes = "사용자 인증 요청을 처리하고 인증 토큰을 반환합니다.")
     fun login(@RequestBody loginDTO: LoginDTO): ResponseEntity<TokenDTO> {
         val userDTO: UserDTO? = userService.login(loginDTO)
 
@@ -26,11 +28,14 @@ class UserController(
     }
 
     @PostMapping(value = ["/join"])
+    @ApiOperation(value = "사용자 등록", notes = "새로운 사용자를 등록합니다.")
     fun join(@RequestBody userDTO: UserDTO): ResponseEntity<Long> = ResponseEntity.ok(userService.join(userDTO))
 
     @PutMapping(value = [""])
+    @ApiOperation(value = "사용자 정보 수정", notes = "등록된 사용자 정보를 수정합니다.")
     fun updateUserInfo(@RequestBody userDTO: UserDTO): ResponseEntity<UserDTO> = ResponseEntity.ok(userService.updateUserInfo(userDTO))
 
     @DeleteMapping(value = [""])
+    @ApiOperation(value = "사용자 탈퇴", notes = "등록된 사용자를 탈퇴 처리합니다.")
     fun withdraw(@RequestBody userDTO: UserDTO): ResponseEntity<Boolean> = ResponseEntity.ok(userService.withdraw(userDTO))
 }
